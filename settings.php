516464c6d8d75819c2689d4b49ecd524c5e2ccd4
Valery Fremaux <valery.fremaux@gmail.com>
2021-09-15 15:39:09 +0200

    automated sync from SVN

--- settings.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$ADMIN->add('users', new admin_externalpage('enroltrainingcreditssettings', get_string('settings'),
-        new moodle_url('/enrol/trainingcredits/usercredits.php'), 'enrol/trainingcredits:managecredits'));
